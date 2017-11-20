197645ee8cf8a61360d3e401fc01665d80b6b24a
angiediedrich <adiedrich@mcad.edu>
2017-11-19 21:28:51 -0600

    not having luck with popovers or toggle with multiple elements

--- script.js
+++ script.js
@@ -1,43 +1,219 @@
-//Clicking on an A element applies a CSS class to it that changes it’s background color
-//Clicking on one of the H2 post titles in the main content area hides it
-
-$(document).ready(function () {
-	//click to addClass that changes background to purple
-    $("a").click(function () {
-        $(this).addClass("changeBackgroundColor");
- 	});
-
-    //hide or slideUp <h2> elements
-    $("h2.title").click(function() {
-      $(this).slideUp("slow", function() {
+$(function() {
+    $('#showdiv1').click(function() {
+        $('div[id^=div]').hide();
+        $('#div1').show();
     });
-	});
-});
-
-//Assignment 11
-//Add the attached icon image before the text in each LI element in the main navigation
-$('#menu ul li a').prepend('<img id="theIcon" src="icon.png" />');
-
-//Clicking on the H1 element in the header changes the background color of the header DIV
-$("h1").click(function () {
-	$("#header").css("background","purple");
-});
-
-//Change the text color of the first LI element in one of the sub-lists in the sidebar
-$('#sidebar_content_1 li a:first').css("color","blue");
-
-//Move the position of the even “numbered” LI elements in one of the sub-lists in the sidebar to the right by 5 pixels
-$('#sidebar_content_1 li a:even').css("margin-left","5px");
-
-//Clicking on one of the post titles in the main content area replaces the content of the content DIV (with the ID of “content”) with content you specify
-$("#content .title").click(function () {
-	$("#content").html("Angie's changing content!");
-	$("#content").css("font-size", "30px")
-});
-//Clicking on one of the H2 elements in the sidebar toggles visibility of the list below it
-$("#sidebar_content_1 h2").click(function() {
-     $(this).next().slideToggle("slow") 
+    $('#showdiv2').click(function() {
+        $('div[id^=div]').hide();
+        $('#div2').show();
     });
-$("#sidebar_content_2 h2").click(function() {
-     $(this).next().slideToggle("slow") 
+ 
+    $('#showdiv3').click(function() {
+        $('div[id^=div]').hide();
+        $('#div3').show();
     });
+ 
+    $('#showdiv4').click(function() {
+        $('div[id^=div]').hide();
+        $('#div4').show();
+    });
+ 
+})
+
+
+
+
+/*$(document).ready(function() {
+
+var modal1 = document.getElementById('modal_1');
+
+var btn1 = document.getElementById("modal_1Btn");
+
+var span1 = document.getElementsByClassName("close")[0];
+
+btn1.onclick = function() {
+modal1.style.display = "block";
+}
+
+span1.onclick = function() {
+modal1.style.display = "none";
+}
+
+window.onclick = function(event) {
+if (event.target == modal) {
+    modal.style.display = "none";
+}
+}
+
+
+var modal2 = document.getElementById('modal_2');
+
+var btn2 = document.getElementById("modal_2Btn");
+
+var span2 = document.getElementsByClassName("close")[0];
+
+btn2.onclick = function() {
+modal2.style.display = "block";
+}
+
+span2.onclick = function() {
+modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal1 || event.target == modal2) {
+    event.target.style.display = "none";
+  }
+}
+}
+/*
+
+// Get the modal
+var modal = document.getElementById('muffinsModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("muffinsBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == modal) {
+modal.style.display = "none";
+}
+}
+
+/*
+// Get the modal
+var applesauceModal = document.getElementById('applesauceModal');
+var muffinsModal = document.getElementById('muffinsModal');
+var turkeyBrineModal = document.getElementById('turkeyBrineModal');
+var whiteChiliModal = document.getElementById('whiteChiliModal');
+var marinatedPorkModal = document.getElementById('marinatedPorkModal');
+var fruitSaladModal = document.getElementById('fruitSaladModal');
+var soupModal = document.getElementById('soupModal');
+
+// Get the button that opens the modal
+var applesauceBtn = document.getElementById("applesauceBtn");
+var muffinsBtn = document.getElementById("muffinsBtn");
+var turkeyBrineBtn = document.getElementById("turkeyBrineBtn");
+var whiteChiliBtn = document.getElementById("whiteChiliBtn");
+var marinatedPorkBtn = document.getElementById("marinatedPorkBtn");
+var fruitSaladBtn = document.getElementById("fruitSaladBtn");
+var soupBtn = document.getElementById("soupBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+applesauceBtn.onclick = function() {
+    applesauceModal.style.display = "block";
+}
+
+muffinsBtn.onclick = function() {
+    muffinsModal.style.display = "block";
+}
+
+turkeyBrineBtn.onclick = function() {
+    turkeyBrineModal.style.display = "block";
+}
+
+whiteChiliBtn.onclick = function() {
+    whiteChiliModal.style.display = "block";
+}
+
+marinatedPorkBtn.onclick = function() {
+    marinatedPorkModal.style.display = "block";
+}
+
+fruitSaladBtn.onclick = function() {
+    fruitSaladModal.style.display = "block";
+}
+
+soupBtn.onclick = function() {
+    soupModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    applesauceModal.style.display = "none";
+}
+
+span.onclick = function() {
+    muffinsModal.style.display = "none";
+}
+
+span.onclick = function() {
+    turkeyBrineModal.style.display = "none";
+}
+
+span.onclick = function() {
+    whiteChiliModal.style.display = "none";
+}
+
+span.onclick = function() {
+    marinatedPorkModal.style.display = "none";
+}
+
+span.onclick = function() {
+    fruitSaladModal.style.display = "none";
+}
+
+span.onclick = function() {
+    soupModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == applesauceModal) {
+        applesauceModal.style.display = "none";
+    }
+}
+
+window.onclick = function(event) {
+    if (event.target == muffinsModal) {
+        muffinsModal.style.display = "none";
+    }
+}
+
+window.onclick = function(event) {
+    if (event.target == turkeyBrineModal) {
+        turkeyBrineModal.style.display = "none";
+    }
+}
+
+window.onclick = function(event) {
+    if (event.target == whiteChiliModal) {
+        whiteChiliModal.style.display = "none";
+    }
+}
+
+window.onclick = function(event) {
+    if (event.target == marinatedPorkModal) {
+        marinatedPorkModal.style.display = "none";
+    }
+}
+
+window.onclick = function(event) {
+    if (event.target == fruitSaladModal) {
+        fruitSaladModal.style.display = "none";
+    }
+}
+
+window.onclick = function(event) {
+    if (event.target == soupModal) {
+        soupModal.style.display = "none";
+    }
+}
